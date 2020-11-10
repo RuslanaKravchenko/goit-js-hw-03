df5cf6239967c5c5794dc48880e71a35980f5a32
Ruslana Kravchenko <ruslana.kravchenko24@gmail.com>
2020-11-10 13:53:43 +0200

    Выполнено первое задание

---
+++ js/task-1.js
@@ -0,0 +1,20 @@
+'use strict'
+let message = '';
+const user = {
+  name: 'Mango',
+  age: 20,
+  hobby: 'html',
+  premium: true,
+};
+
+user.mood = 'happy';
+user.hobby = 'skydiving';
+user.premium = false;
+
+const keys = Object.keys(user);
+for (const key of keys) {
+  message +=`${key}: ${user[key]}\n`;
+ 
+ }
+
+ console.log(message);
