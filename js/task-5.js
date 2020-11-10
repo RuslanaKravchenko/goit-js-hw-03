52370bd425bb8126081d30403d3d80fdc1081b61
Ruslana Kravchenko <ruslana.kravchenko24@gmail.com>
2020-11-10 15:18:40 +0200

    Выполнено задание 5

--- js/task-5.js
+++ js/task-5.js
@@ -1,1 +1,30 @@
 'use strict'
+const products = [
+  { name: 'Радар', price: 1300, quantity: 4 },
+  { name: 'Сканер', price: 2700, quantity: 3 },
+  { name: 'Дроид', price: 400, quantity: 7 },
+  { name: 'Захват', price: 1200, quantity: 2 },
+];
+
+const getAllPropValues = function(array, prop) {
+ const result = [];
+  
+  for (const object of array) {
+    for(const key in object) {
+      if (key === prop) {
+        result.push(object[key]);
+      }
+      
+    }
+  }
+  return  result;
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(getAllPropValues(products, 'name')); // ['Радар', 'Сканер', 'Дроид', 'Захват']
+
+console.log(getAllPropValues(products, 'quantity')); // [4, 3, 7, 2]
+
+console.log(getAllPropValues(products, 'category')); // []
