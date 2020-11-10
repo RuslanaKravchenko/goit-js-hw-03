a216ecb250c28aad64c19017ecc2e7e859712246
Ruslana Kravchenko <ruslana.kravchenko24@gmail.com>
2020-11-10 14:07:10 +0200

    Выполнено задание 2

---
+++ js/task-2.js
@@ -0,0 +1,11 @@
+'use strict'
+const countProps = function(obj) {
+const values = Object.keys(obj);
+  return values.length;};
+
+
+console.log(countProps({})); // 0
+
+console.log(countProps({ name: 'Mango', age: 2 })); // 2
+
+console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 })); // 3
