db0d5c313d15380d95f490d39fc2f2b7fc120c4e
Ruslana Kravchenko <ruslana.kravchenko24@gmail.com>
2020-11-10 14:57:42 +0200

    Выполнено задание 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,43 @@
 'use strict'
+// const countTotalSalary = function(employees) {
+//  const salaryArr = Object.values(employees);
+//   let total = 0;
+
+//   for (const salary of salaryArr) {
+//     total += salary;
+//   }
+//   return total;
+// };
+
+
+// =============Вариант второй==========
+
+const countTotalSalary = function(employees) {
+  let total = 0;
+  for ( const key in employees) {
+    total += employees[key];
+  }
+  return total;
+
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(countTotalSalary({})); // 0
+
+console.log(
+  countTotalSalary({
+    mango: 100,
+    poly: 150,
+    alfred: 80,
+  }),
+); // 330
+
+console.log(
+  countTotalSalary({
+    kiwi: 200,
+    lux: 50,
+    chelsy: 150,
+  }),
+); // 400
